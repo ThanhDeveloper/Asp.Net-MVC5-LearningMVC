22ffab8b5087d63872c38a4f8d07ac17eee43d96
Thanhdev1998 <thanhdev98@gmail.com>
2020-03-27 18:04:56 +0700

    no message

---
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers from db
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext(); 
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+        public ViewResult Index()
+        {
+            var customers = GetCustomer();
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Customers = (List<Customers>)customers
+            //};
+            //return View(viewModel);
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomer().SingleOrDefault(c => c.Id == id);
+            if(customer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(customer);
+        }
+
+        //Init customer
+        private IEnumerable<Customers> GetCustomer()
+        {
+            return new List<Customers>
+            {
+                new Customers {Id = 1 , Name = "John Smith"},
+                new Customers {Id = 2 , Name = "Mary Williams"}
+            };
+        }
+    }
+}
---
+++ Vidly/Vidly/Models/Customers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    public class Customers
+    {
+        public int Id { set; get; }
+        [Required] //overide and set column name not null
+        [StringLength(255)] //overide and set length
+        public string Name { set; get; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+
+        public byte MembershipTypeID { get; set; }
+    }
+}
